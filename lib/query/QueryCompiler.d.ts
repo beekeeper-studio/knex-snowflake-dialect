ea0c450a442aa02f8425150c42d81301281e0cb9
Day Matchullis <dsm6069@gmail.com>
2026-05-14 13:07:09 -0600

    add workflows, remove build artifacts

--- lib/query/QueryCompiler.d.ts
+++
@@ -1,6 +0,0 @@
-import QueryCompiler_MySQL from "knex/lib/dialects/mysql/query/mysql-querycompiler";
-export declare class QueryCompiler extends QueryCompiler_MySQL {
-    constructor(client: any, builder: any, formatter: any);
-    forUpdate(): string;
-    forShare(): string;
-}
